f27731be3f49be1bcb32d6f429947125328a0bd2
Ike <ikescode@gmail.com>
2020-03-06 13:24:20 -0800

    Updates

---
+++ precache-manifest.02ba525e5b98a4d4e05c2dc47a378a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9b062131d02fa29ab6fca617fe6606",
+    "url": "/react-task-list/index.html"
+  },
+  {
+    "revision": "ae1d35e47b085704a606",
+    "url": "/react-task-list/static/css/main.766fc1c2.chunk.css"
+  },
+  {
+    "revision": "49c3a886c2623bd4d856",
+    "url": "/react-task-list/static/js/2.87945d08.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-task-list/static/js/2.87945d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1d35e47b085704a606",
+    "url": "/react-task-list/static/js/main.0cb97daa.chunk.js"
+  },
+  {
+    "revision": "7729a87fac1a5b962176",
+    "url": "/react-task-list/static/js/runtime-main.764980a4.js"
+  }
+]);
